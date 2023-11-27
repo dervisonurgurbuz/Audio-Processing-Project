5d97b75614ff43b5e7a26f68fda1e539edc2f306
dervisonurgurbuz <onurgurbuz97@gmail.com>
2023-11-27 18:22:33 +0100

    Added emotion tags to client

--- server.js
+++ server.js
@@ -6,16 +6,16 @@ const app = express();
 const port = 3004
 
 app.get('/', (req, res) => {
-    console.log(req.query)
-
+   
     let emotionTag = req.query
+    console.log(emotionTag)
+
     res.send('Request have been received :) ')
 
   })
 
 
 app.listen(port, () => {
-
 
     console.log(`App listening port: ${port}`)
   })
