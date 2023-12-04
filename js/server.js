e1780018f5e81481d655867f456579dbc28bfe81
dervisonurgurbuz <onurgurbuz97@gmail.com>
2023-12-04 22:30:11 +0100

    final commit before demo day

--- js/server.js
+++ js/server.js
@@ -6,14 +6,20 @@ const app = express();
 const port = 3004
 
 app.get('/', (req, res) => {
-   
+    let data = ''
     let emotionTag = req.query
     // console.log(emotionTag)
     if(emotionTag.emotion === 'Happy' || emotionTag.emotion === 'Calm' || emotionTag.emotion === 'Energetic' || emotionTag.emotion === 'Sad'){
       const musicList = fs.readFileSync('./data_moods.csv',
       { encoding: 'utf8', flag: 'r' });
-      let data = searchCSV(musicList,emotionTag.emotion,emotionTag.tempo)
+      data = searchCSV(musicList,emotionTag.emotion,emotionTag.tempo)
       // console.log(parseCSV(musicList))
+      data = JSON.stringify(data)
+
+      fs.writeFileSync("musiclist.json", data)        
+     
+             
+         
       res.send(data)
     }else{
       res.status(500).send('Emotion Tag is not defined')
@@ -29,9 +35,10 @@ app.listen(port, () => {
 
      // Function to parse CSV data
 function searchCSV(csvData,emotionTag,tempo) {
-  const rows = csvData.split('\r\n'); // Split the CSV data into rows
-  const headers = rows[0].split(','); // Extract headers
-  const data = [];
+  let rows = csvData.split('\r\n'); // Split the CSV data into rows
+  let headers = rows[0].split(','); // Extract headers
+  const data = []; 
+  let music = {};
 
   for (let i = 1; i < rows.length; i++) {
     const row = rows[i].split(','); // Split each row into values
@@ -41,7 +48,15 @@ function searchCSV(csvData,emotionTag,tempo) {
         obj[headers[j]] = row[j]; // Create an object using header-value pairs
         // console.log(obj.mood)
         if(obj.mood===emotionTag){
-        data.push(obj); // Push the object to the data array
+          
+          music = {
+            "name": obj.name,
+            "album": obj.album,
+            "artist": obj.artist,
+            "tempo": obj.tempo
+
+          }
+          data.push(music); // Push the object to the data array
         }
       }      
       if(tempo<100){
